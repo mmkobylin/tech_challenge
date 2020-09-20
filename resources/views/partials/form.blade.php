d41db896f69af44894a958e22cd7fa832f1aa953
mmkobylin <mmkobylin@gmail.com>
2020-09-20 09:13:13 +0100

    method in form changed to get as the players name should be visible to everyone

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -4,11 +4,8 @@
     @section("title")
     
     @section("content")
-        <h1>Player's name</h1>
     
-        <form method="post" class="form card">
-                <h2 class="card-header">Join us</h2>
-                    <fieldset class="card-body">
+        <form method="get" class="form card">
                     @csrf
                         <div class="form-group @error('title') is-invalid @enderror>
                             @error("first_name")
@@ -19,7 +16,6 @@
                             <label for="first_name">Player's Name</label>
                             <input id="first_name" name="first_name" class="form-control" value="{{ old("first_name") }}" />
                         </div>
-                    </fieldset>
                     <div class="card-footer text-right">
                         <button class="btn btn-success">Submit</button>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::post('create', 'Players@createPost');
+    Route::get('{player}', "Player@show");
 });
