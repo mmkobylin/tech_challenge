33f4737b197151a6f4ff593883a85f3909e2e1e5
mmkobylin <mmkobylin@gmail.com>
2020-09-24 20:24:51 +0100

    score input with disable option implemented from score-branch

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -53,22 +53,43 @@ collect(array_push($allPlayers, $Player1, $Player2, $Player3, $Player4));
 //this is where i shuffle and collect!
 $randomPlay = (collect($allPlayers)->shuffle()->all());
 ?>
-<section class=matches>
-        <div class="column-results"><br>
-        {{-- are players randomize and present? if so, we have a match --}}
-            <h3 class="match"><?php print_r(($randomPlay[0] !== NULL && $randomPlay[1] !== NULL ) ?  'MATCH 1 :' : NULL ) ?></h3>
-            {{-- this is where we display player - if it is NULL, it does not display anything --}}
-                <p class="player"><?php print_r($randomPlay[0]) ?></p>
-            {{-- if we dont have a match, we don't need versus --}}
-                <p class="versus"><?php print_r(($randomPlay[0] === NULL || $randomPlay[1] === NULL ) ?  'Please enter the names' : ' vs ') ?></p>
-                <p class="player"><?php print_r($randomPlay[1]) ?></p>
+    <section class="column">
+    {{-- are players randomize and present? if so, we have a match --}}
+        <h3 class="match"><?php print_r(($randomPlay[0] !== NULL && $randomPlay[1] !== NULL ) ?  'MATCH 1 :' : NULL ) ?></h3>
+    {{-- this is where we display player - if it is NULL, it does not display anything --}}
+        <div class="player">
+            <p><?php print_r($randomPlay[0]) ?></p>
+            {{-- this is where we are displaying the scores --}}
+            <input type="number" id="score" name="score" placeholder="0" min="0" max="21"
+            {{-- this is where we disable the button if the input have not been submitted --}}
+            <?php if ($randomPlay[1] == NULL){ ?> disabled <?php   } ?>>
         </div>
-        <div class="column-results"><br>
-            <h3 class="match"><?php print_r(($randomPlay[2] !== NULL && $randomPlay[3] !== NULL ) ?  'MATCH 2 :' : NULL ) ?></h3>
-                <p class="player"><?php print_r($randomPlay[2]) ?></p>
-                <p class="versus"><?php print_r(($randomPlay[2] === NULL || $randomPlay[3] === NULL ) ?  " click 'Randomise?' to play!" : ' vs ') ?></p>
-                <p class="player"><?php print_r($randomPlay[3]) ?></p>
+    {{-- if we dont have a match, we don't need versus --}}
+        <p class="versus"><?php print_r(($randomPlay[0] === NULL || $randomPlay[1] === NULL ) ?  'Please enter the names' : ' vs ') ?></p>
+            <div class="player">
+                <p><?php print_r($randomPlay[1]) ?></p>
+                <input type="number" id="score" name="score" placeholder="0" min="0" max="21"
+                <?php if ($randomPlay[1] == NULL){ ?> disabled <?php   } ?>>
+            </div>
+
         </div>
+    </section>
+    <section class="column">
+        <h3 class="match"><?php print_r(($randomPlay[2] !== NULL && $randomPlay[3] !== NULL ) ?  'MATCH 2 :' : NULL ) ?></h3>
+            <div class="player">
+                <p><?php print_r($randomPlay[2]) ?></p>
+                <input type="number" id="score" name="score" placeholder="0" min="0" max="21"
+                <?php if ($randomPlay[2] == NULL){ ?> disabled <?php   } ?>>
+            </div>
+            
+            <p class="versus"><?php print_r(($randomPlay[2] === NULL || $randomPlay[3] === NULL ) ?  " click 'Randomise?' to play!" : ' vs ') ?></p>
+            
+            <div class="player">
+                <p><?php print_r($randomPlay[3]) ?></p>
+                <input class="score" type="number" id="score" name="score" placeholder="0" min="0" max="21"
+                <?php if ($randomPlay[3] == NULL){ ?> disabled <?php   } ?>>
+                 
+            </div>
     </section>
 
 </body>
